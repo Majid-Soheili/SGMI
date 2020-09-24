913084850433c900d854af4363257582829185b6
Hichagha <hichagha@gmail.com>
2020-09-24 13:42:23 +0330

    Create test_SR.scala

---
+++ src/test/scala/org/apache/spark/ml/feature/OptimizationMethods/test_SR.scala
@@ -0,0 +1,21 @@
+package org.apache.spark.ml.feature.OptimizationMethods
+
+import org.apache.spark.ml.feature.BasicTest
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class test_SR extends BasicTest {
+  test("SR-Q") {
+
+    val path = "src/test/scala/resources/data/Q.csv"
+    val source = scala.io.Source.fromFile(path)
+    val lines = source.getLines().toArray
+    val Q = lines.map(s => s.split(",").map(_.toDouble))
+    val weights = SR(Q, true, false).getWeights
+    val actualRank = weights.zipWithIndex.sortBy(_._1).reverse.map(_._2)
+    val expectedRank = Array(10, 11, 9, 8, 14, 15, 5, 16, 12, 4, 6, 13, 7, 3, 17, 18, 2, 19, 0, 1, 20)
+
+    assert(actualRank === expectedRank)
+  }
+}
